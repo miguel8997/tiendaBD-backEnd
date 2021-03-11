49dfa2dd2e2bd82bbff803b4010fcbea042b2723
miguel8997 <38220141+miguel8997@users.noreply.github.com>
2021-03-11 00:21:44 -0300

    Feature/modelo (#2)
    * commit
    
    * modelo creado

--- src/main/java/cl/dci/dbtienda/modelo/Tienda.java
+++ src/main/java/cl/dci/dbtienda/modelo/Tienda.java
@@ -1,4 +1,42 @@
 package cl.dci.dbtienda.modelo;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Set;
+
+@Entity
+@Table(	name = "tienda",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "nombre")
+        })
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Tienda {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(max = 30)
+    private String nombre;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id")
+    @JsonIgnoreProperties("tiendas")
+    private Usuario usuario_id;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "tienda_id")
+    @JsonIgnoreProperties("producto")
+    private Set<Producto> productos;
+
+
+
 }
--- src/main/java/cl/dci/dbtienda/modelo/Usuario.java
+++ src/main/java/cl/dci/dbtienda/modelo/Usuario.java
@@ -1,15 +1,23 @@
 package cl.dci.dbtienda.modelo;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.Set;
 
 @Entity
 @Table(	name = "usuario",
         uniqueConstraints = {
                 @UniqueConstraint(columnNames = "correo")
         })
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Usuario {
 
         @Id
@@ -23,5 +31,9 @@ public class Usuario {
         @NotBlank
         @Size(max = 30)
         private String clave;
+
+        @OneToMany(cascade = CascadeType.ALL, mappedBy = "usuario_id")
+        @JsonIgnoreProperties("usuario")
+        private Set<Tienda> tiendas;
 
 }
